17f365cbace36445ca0bb9dc6f02ff0a1642ecac
sanciyuandehundan <113023820+sanciyuandehundan@users.noreply.github.com>
2023-03-05 18:15:24 +0800

    添加项目文件。

---
+++ dllmain.cpp
@@ -0,0 +1,10 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+extern "C" _declspec(dllexport) void midi_play(int yuepu[], HMIDIOUT midiout)
+{
+    //midiOutShortMsg(midiout, 0x7e << 16 | 70 << 8 | 0x90);
+    Music music = Music();
+    music.play(yuepu, midiout);
+}
+
---
+++ pch.h
@@ -0,0 +1,29 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+#include"array"
+#include"iostream"
+#include "wincrypt.h"
+#include"mmeapi.h"
+#include"Windows.h"
+#include"Mmsystem.h"
+#pragma comment(lib,"winmm.lib")
+using namespace std;
+
+#endif ;//PCH_H
+
+class Music {
+public:
+	//MIDIHDR midihdr;//存储
+	//HANDLE handle;//缓冲区句柄
+	Music();//建构函数
+	void play(int g[], HMIDIOUT a);//演奏
+};
