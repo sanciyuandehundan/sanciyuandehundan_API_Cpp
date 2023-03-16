9569e0682b8f7efa39f5debc3a020bf454668b6d
一 吴 <a0905591936@gmail.com>
2023-03-16 22:48:57 +0800

    1

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,17 +8,21 @@
 #include"Windows.h"
 #include"mmsystem.h"
 #pragma comment(lib,"winmm.lib")
-using namespace std;
-extern "C" _declspec(dllexport) void midi_play(int yuepu[], HMIDIOUT handle)
+//using namespace std;
+extern "C" _declspec(dllexport) void midi_play()
 { 
     //midiOutShortMsg(handle, 0x7e << 16 | 60 << 8 | 0x90);
     /*Music music = Music();//初始化music实例
     music.play_mid(handle);*/
     //mciSendStringA("open C:\\Users\\a0905\\Desktop\\mydata.mid type sequencer alias m1", 0, 0, 0);
-    mciSendStringA("open C:\\Users\\a0905\\Desktop\\mydata.mid", 0, 0, 0);
+    if (0 == mciSendString(L"open C:\\Users\\a0905\\Downloads\\yingui_all.MID type sequencer Alias midi", NULL, 0, NULL)) {
+        cout << "s";
+        //midiOutShortMsg(handle, 0x7e << 16 | 60 << 8 | 0x90);
+        mciSendString(L"play midi", NULL, 0, NULL);
+        Sleep(1000000);
+    }
     //mciSendStringW("open C:\Users\a0905\source\repos\sanciyuandehundan\music\bin\Debug\yingui0_1.mid",0,0,0);
     //Sleep(5000);
-    mciSendStringA("play m1", 0, 0, 0);
     //delete &music;//删除music实例
     /*MCI_OPEN_PARMS OpenParms;
     OpenParms.lpstrDeviceType =(LPCWSTR)MCI_DEVTYPE_SEQUENCER;//MIDI类型
